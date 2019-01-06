b17c6459d43946dba11eb6aac9956cacc8aa2c6c
Andrii Otroshko <otroshko@gmail.com>
2019-01-06 15:40:23 +0200

    Added solution for problem #88

--- java/src/_0088/MergeSortedArray.java
+++ java/src/_0088/MergeSortedArray.java
@@ -11,9 +11,22 @@ package _0088;
     You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
 */
 
-
 public class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // TODO: Add solution.
+        if(n < 1) {
+            return;
+        }
+
+        int idx1 = m - 1, idx2 = n - 1, currentPosition = m + n - 1;
+
+        while(idx1 >=0 && idx2 >= 0) {
+            nums1[currentPosition--] = nums1[idx1] > nums2[idx2]
+                    ? nums1[idx1--]
+                    : nums2[idx2--];
+        }
+
+        while(idx2 >= 0) {
+            nums1[currentPosition--] = nums2[idx2--];
+        }
     }
 }
--- java/src/_0088/MergeSortedArrayTest.java
+++ java/src/_0088/MergeSortedArrayTest.java
@@ -22,4 +22,52 @@ public class MergeSortedArrayTest {
         // Assert
         assertArrayEquals(expected, nums1);
     }
+
+    @Test
+    public void test2() {
+        // Arrange
+        int[] expected = new int[] { 1 };
+        int[] nums1 = new int[] { 1 };
+        int m = 1;
+        int[] nums2  = new int[0];
+        int n = 0;
+
+        // Act
+        _solution.merge(nums1, m, nums2, n);
+
+        // Assert
+        assertArrayEquals(expected, nums1);
+    }
+
+    @Test
+    public void test3() {
+        // Arrange
+        int[] expected = new int[] { 1,2,3,4,5,6 };
+        int[] nums1 = new int[] { 1,2,4,5,6,0 };
+        int m = 5;
+        int[] nums2  = new int[] { 3 };
+        int n = 1;
+
+        // Act
+        _solution.merge(nums1, m, nums2, n);
+
+        // Assert
+        assertArrayEquals(expected, nums1);
+    }
+
+    @Test
+    public void test4() {
+        // Arrange
+        int[] expected = new int[] { 1,2,3,4,5,6 };
+        int[] nums1 = new int[] { 4,5,6,0,0,0 };
+        int m = 3;
+        int[] nums2  = new int[] { 1,2,3 };
+        int n = 3;
+
+        // Act
+        _solution.merge(nums1, m, nums2, n);
+
+        // Assert
+        assertArrayEquals(expected, nums1);
+    }
 }
